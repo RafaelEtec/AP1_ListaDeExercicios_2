b61b9196f828107de07b1f842888d478008130fa
rafael.fgoulart1 <rafael.fgoulart1@CAS478704W1006.senacsp.edu.br>
2022-03-14 19:55:07 -0300

    Atividade Iniciada

---
+++ src/main/java/Parte_1/Exercicios1a5.java
@@ -0,0 +1,12 @@
+package Parte_1;
+import java.util.Scanner;
+/** 
+   por Rafael Ferreira Goulart
+**/
+public class Exercicios1a5 {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        
+        
+    }
+}
